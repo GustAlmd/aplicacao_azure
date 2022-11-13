b3f1ad705752d4ba090ae38f57af085e812e6a0c
GustAlmd <gustavo.almd@outlook.com>
2022-11-13 13:45:47 -0300

    commit primary

---
+++ header.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Azure Hospedagens</h1>
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicação na Azure</title>
+</head>
+<body>
+    <?php include_once('header.php') ?> 
+    <?php 
+        echo "Gustavo Almeida";
+    ?>   
+    <?php include_once('footer.php') ?>
+</body>
+</html>
